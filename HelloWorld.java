17f83497634a587e08cb69ba3685726256c377ee
Moksahu <msahu7833@gmail.com>
2023-08-22 10:33:49 +0530

    Code for testing

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("World is full of miracle");
+        System.out.println("We should do something to save this world");
+
+    }
+}
